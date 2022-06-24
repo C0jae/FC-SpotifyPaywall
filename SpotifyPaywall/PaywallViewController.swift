3e40a143e4e631981f89beb7846cbfb15806993c
Youngjae Choi <cdw0807@naver.com>
2022-06-24 16:10:36 +0900

    스토리보드 구성

--- SpotifyPaywall/PaywallViewController.swift
+++ SpotifyPaywall/PaywallViewController.swift
@@ -12,8 +12,20 @@ import UIKit
 // 과제: 아래 애플 샘플 코드 다운받아서 돌려보기  https://developer.apple.com/documentation/uikit/views_and_controls/collection_views/implementing_modern_collection_views
 
 class PaywallViewController: UIViewController {
-
-     override func viewDidLoad() {
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var pageController: UIPageControl!
+    
+    let bannerInfos: [BannerInfo] = BannerInfo.list
+    let colors: [UIColor] = [.systemRed, .systemBlue, .systemPink, .systemPurple]
+    var datasource: UICollectionViewDiffableDataSource<Section, Item>!
+    
+    typealias Item = BannerInfo
+    enum Section {
+        case main
+    }
+    
+    
+    override func viewDidLoad() {
         super.viewDidLoad()
     }
 }
